ac5e697637191eadfefba59ef3eb68fc773c09ab
mogran <lumingliang.github@gmail.com>
2016-09-20 17:06:50 +0800

    add nand flash register redefine

---
+++ bootstrap.h
@@ -0,0 +1,16 @@
+#ifndef BOOTSTARP_H__
+#define BOOTSTARP_H__
+
+typedef unsigned int  uint32_t
+typedef unsigned char uint8_t
+
+
+#define NFCONT (*(volatile unsigned int*)0x48000004)
+
+
+
+#endif
+
+
+
+
