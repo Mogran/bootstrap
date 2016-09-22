2880b1521a0b8e1cdb6292a4b80d10c8fa633eb3
mogran <lumingliang.github@gmail.com>
2016-09-22 16:47:47 +0800

    fixed uart

--- uart.c
+++ uart.c
@@ -1,12 +1,20 @@
 #include "bootstrap.h"
 
-
 void uart_init(void)
 {
+	/* uart0 */
 	GPHCON &= ~((3 << 2) | (3 << 0));
 	GPHCON |= (2 << 2) |(2 << 0); 
+
+	/* uart3 */
+	GPHCON &= ~((3 << 14) | (3 << 12));
+	GPHCON |= (2 << 14) |(2 << 12); 
+
 	GPHUDP &= ~((3 << 2) | (3 << 0));
 	GPHUDP |= (2 << 2) |(2 << 0); 
+
+	GPHUDP &= ~((3 << 14) | (3 << 12));
+	GPHUDP |= (2 << 14) |(2 << 12); 
 
 	ULCON0 = 0x3;
 	UCON0  = 0x5;
@@ -21,8 +29,10 @@ void uart_tx_byte(void)
 	volatile int i = 0;
 
 	for(i = 0; i < 6; i++){
-		wait4txdone();
+		while(!(UTRSTAT0&0x2));
 		UTXH0 = 0x55;
 	}
+
+	led_display();
 }
 
